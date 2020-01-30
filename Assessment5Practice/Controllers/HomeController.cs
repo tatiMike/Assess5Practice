a531d620be7778ad591133034faa1754036c0011
M B <mbon419@gmail.com>
2020-01-30 16:12:44 -0500

    first

---
+++ Assessment5Practice/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Assessment5Practice.Models;
+
+namespace Assessment5Practice.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult LoginUser(User u)
+        {
+            return View(u);
+        }
+
+        public IActionResult RegisteredOrFailedToRegisterViews(User u)
+        {
+            
+            string password = "open sesame";
+
+            if (password == "open sesame")
+            {
+                return Registered();
+            }
+            else
+            {
+                return LoginUser(u);
+            }
+        }
+
+        public IActionResult Registered()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
